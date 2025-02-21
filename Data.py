31ec93d21900ac7d7a632a6211c7dd5da720e4cb
Grizzlazy <134766687+Grizzlazy@users.noreply.github.com>
2025-02-21 14:06:08 +0700

    Run-100-A=2

--- Data.py
+++ Data.py
@@ -7,12 +7,12 @@ import time
 import itertools
 import collections
 
-number_of_trucks = 2
+number_of_trucks = 3
 number_of_cities = 0
-number_of_drones = 2
+number_of_drones = 3
 truck_speed = 0.5
 drone_speed = 1
-drone_capacity = 4
+drone_capacity = 2
 drone_limit_time = 90
 city_demand = []
 release_date = []
@@ -27,7 +27,7 @@ B_ratio = 0.7
 C_ratio = 0.1
 delta = 0.6
 alpha = [0.3, 0.2, 0.1]
-theta = 1
+theta = 2
 
 
 def read_data(path):
